3a7125083e8ab8dc50ef5bc571307f64e900c0a4
teamkoregbgb <teamkoregbgb@gmail.com>
2018-03-26 00:05:52 +0200

    ett test

--- app/src/main/java/dev/wardrobeapp/MainActivity.java
+++ app/src/main/java/dev/wardrobeapp/MainActivity.java
@@ -1,5 +1,7 @@
 package dev.wardrobeapp;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent,0);
+        int i;
     }
 }
